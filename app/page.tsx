a8fb244234d14bb0690f76a66a86f13e016a45da
maneesh <maneesh085@gmail.com>
2025-02-04 01:22:34 +0530

    added navbar in header

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="row-start-1 flex items-center gap-4">
-      <NavigationMenuDemo />
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-        <h1 className="text-2xl font-bold">Welcome to Hire Mate</h1>
+      <div className="flex items-center gap-4">
+        <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /><h1 className="text-2xl font-bold">Welcome to Hire Mate</h1>
+      </div>
+      <div className="flex gap-4">
+        <NavigationMenuDemo />
+      </div>
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
